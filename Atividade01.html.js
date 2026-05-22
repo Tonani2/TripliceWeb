da1402ab1730ef07c2404e5c1b36243e57e275bc
Ana Tonani <tonanianaluiza2@gmail.com>
2026-05-21 21:12:56 -0300

    O Usuário pode alterar a cor do Botão quando ele quiser

--- Atividade01.html.js
+++ Atividade01.html.js
@@ -6,14 +6,24 @@ const elementoColor = document.getElementById("color");
 elementoColor.addEventListener("input", (e) => {
      let corNova = e.target.value;
 
+     localStorage.setItem("primaryColor", corNova);
+
     document.documentElement.style.setProperty("--primary-color", corNova);
 });
 
 document.addEventListener("DOMContentLoaded", () =>
 {
+    if (localStorage.getItem("primaryColor")){
+        elementoColor.value = localStorage.getItem("primaryColor");
+
+        document.documentElement.style.setProperty("--primary-color", elementoColor.value);
+    } else {
+
    const rootStyle = window.getComputedStyle(document.documentElement);
 
    const  primaryColor = rootStyle.getPropertyValue("--primary-color").trim();
 
    elementoColor.value = primaryColor;
+
+    }
 });
